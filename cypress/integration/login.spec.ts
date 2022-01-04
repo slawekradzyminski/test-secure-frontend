e1480f9a0d7ee3cc8cfbe1f57dfff5204ab7feab
Slawek <s.radzyminski@ocado.com>
2022-01-04 11:38:41 +0100

    work progress

---
+++ cypress/integration/login.spec.ts
@@ -0,0 +1,16 @@
+
+/// <reference types="cypress" />
+
+describe('Login page', () => {
+    beforeEach(() => {
+      cy.visit('')
+    })
+  
+    it('should successfully login', () => {
+        cy.get('[name=username]').type('admin')
+        cy.get('[name=password]').type('admin')
+        cy.get('.btn-primary').click()
+        cy.get('h1').should('contain.text', 'Slawomir')
+    })
+  })
+  
---
+++ cypress/integration/register.spec.ts
@@ -0,0 +1,21 @@
+
+/// <reference types="cypress" />
+
+import { getRandomString } from "../util/random"
+
+describe('Register page', () => {
+    beforeEach(() => {
+      cy.visit('/register')
+    })
+  
+    it('should successfully login', () => {
+        cy.get('[name=username]').type(getRandomString())
+        cy.get('[name=password]').type(getRandomString())
+        cy.get('[name=firstName]').type(getRandomString())
+        // cy.get('[name=lastName]').type(getRandomString())
+        cy.get('[name=email]').type(`${getRandomString()}@ocado.com`)
+        cy.get('.btn-primary').click()
+        cy.get('.alert').should('contain.text', 'Registration successful')
+    })
+  })
+  
