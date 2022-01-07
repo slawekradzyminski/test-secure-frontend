0a4ae2786733fc9cd70b06bbb6fdb9e792310b2c
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-01-07 12:38:09 +0100

    added Allure reporting

--- README.md
+++ README.md
@@ -27,3 +27,10 @@ Application should run on [http://localhost:8080](http://localhost:8080)
 ```commandline
 npm start & ./node_modules/.bin/wait-on http://localhost:8080 && npx cypress run --browser chrome --headed
 ```commandline
+
+## allure
+
+```commandline
+npm run
+allure serve
+```
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,16 +1,5 @@
+/// <reference types="@shelex/cypress-allure-plugin" />
 /// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
 
 /**
  * @type {Cypress.PluginConfig}
@@ -18,9 +7,7 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   require('@cypress/code-coverage/task')(on, config)
-  // include any other plugin code...
+  require('@shelex/cypress-allure-plugin/writer')(on, config);
 
-  // It's IMPORTANT to return the config object
-  // with any changed environment variables
   return config
 }
