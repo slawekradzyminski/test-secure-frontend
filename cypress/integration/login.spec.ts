65108ae7ce880274bbe687d3ddd67ab7481d1c23
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-02-10 14:09:33 +0100

    ts

---
+++ cypress/integration/login.spec.ts
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Login page', () => {
+    beforeEach(() => {
+        cy.visit('')
+    })
+
+    it('should successfully login', () => {
+        cy.get('[name=username]').type('admin')
+        cy.get('[name=password]').type('admin')
+        cy.get('.btn-primary').click()
+        cy.get('h1').should('contain.text', 'Hi Slawomir')
+    })
+
+    it('should fail to login', () => {
+        cy.get('[name=username]').type('wrong')
+        cy.get('[name=password]').type('wrong')
+        cy.get('.btn-primary').click()
+        cy.get('.alert').should('have.text', 'Invalid username/password supplied')
+            .should('have.class', 'alert-danger')
+    })
+
+})
