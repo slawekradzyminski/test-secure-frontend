9d615b1a41f34a3b600cbc57dbe56c3e7c515478
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-02-17 11:04:39 +0100

    play35

--- cypress/integration/intercept/login.intercept.spec.ts
+++ cypress/integration/intercept/login.intercept.spec.ts
@@ -10,15 +10,21 @@ const homePageAssertions = new HomePageAssertions()
 const user = getUser()
 
 describe('Login page with mocks', () => {
+    const users = require("../../fixtures/users.json")
+
     beforeEach(() => {
         cy.visitWithNoMarketing(user, '')
     })
 
     it('should successfully login', () => {
-        
-
         loginPage.login(user.username, user.password)
         homePageAssertions.verifyHeader(user.firstName)
+        cy.get('ul li').should('have.length', users.length)
+        cy.get('ul li').first().should('contain.text', `${users[0].firstName} ${users[0].lastName}`)
+    })
+
+    it('should fail to login', () => {
+        loginPage.login('wrong', 'wrong')
     })
 
 })
