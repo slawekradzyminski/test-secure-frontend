8b069edd1bb12e43d1463c2637a8abb28025afa4
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-02-17 15:34:50 +0100

    play39

---
+++ cypress/integration/iframe.spec.ts
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Iframe', () => {
+    beforeEach(() => {
+        cy.visit('./iframe.html')
+    })
+
+    it('should successfully register & the login', {baseUrl: null}, () => {
+        
+    })
+
+})
