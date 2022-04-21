4dd0c43a0e14e54a39cbe2576f4270fd80d76cf6
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-04-21 13:47:22 +0200

    parallel execution

---
+++ cypress/integration/integration copy 2/login.spec.js
@@ -0,0 +1,65 @@
+/// <reference types="cypress" />
+
+import { getRandomString } from "../../util/random"
+
+describe('login page', () => {
+  beforeEach(() => {
+    cy.visit('')
+  })
+
+  it('should successfully login', () => {
+    const username = getRandomString()
+    const password = getRandomString()
+    const firstName = getRandomString()
+
+    cy.intercept('POST', '**/users/signin', {
+      statusCode: 200,
+      body: {
+        username: username,
+        roles: ['ROLE_ADMIN'],
+        firstName: firstName,
+        lastName: 'Radzyminski',
+        token: 'fakeToken',
+        email: 'admin@email.com'
+      }
+    }).as('loginRequest')
+    cy.intercept('GET', '**/users', { fixture: 'users.json' })
+
+    cy.get('[name=username]').type(username)
+    cy.get('[name=password]').type(password)
+    cy.get('.btn-primary').click()
+
+    cy.get('h1').should('contain.text', firstName)
+
+    cy.wait('@loginRequest').its('request.body').should('deep.equal', { username, password })
+  })
+
+  it('should fail to login', () => {
+    const message = 'Invalid username/password supplied'
+
+    cy.intercept('POST', '**/users/signin', {
+      statusCode: 422,
+      body: {
+        timestamp: '2022-04-20T09:36:10.744+00:00',
+        status: 422,
+        error: 'Unprocessable Entity',
+        message: message,
+        path: '/users/signin'
+      }
+    })
+    cy.get('[name=username]').type(getRandomString())
+    cy.get('[name=password]').type(getRandomString())
+    cy.get('.btn-primary').click()
+
+    cy.get('.alert').should('have.text', message)
+  })
+
+  it('should display loading indicator', () => {
+    cy.intercept('POST', '**/users/signin', { delay: 1000 })
+    cy.get('[name=username]').type(getRandomString())
+    cy.get('[name=password]').type(getRandomString())
+    cy.get('.btn-primary').click()
+
+    cy.get('.btn-primary .spinner-border').should('be.visible')
+  })
+})
---
+++ cypress/integration/integration copy 4/edit.spec.js
@@ -0,0 +1,60 @@
+/// <reference types="cypress" />
+
+import users from '../../fixtures/users.json'
+import { getRandomEmail, getRandomString } from '../../util/random'
+
+describe('edit page', () => {
+    const firstUser = users[0]
+
+    beforeEach(() => {
+        const user = {
+            roles: ['ROLE_ADMIN'],
+            token: 'fakeToken',
+        }
+        window.localStorage.setItem('user', JSON.stringify(user))
+        cy.intercept('GET', '**/users', { fixture: 'users.json' })
+        cy.intercept('GET', `**/users/${firstUser.username}`, { body: firstUser })
+        cy.visit('')
+    })
+
+    it('should correctly fill data', () => {
+        // when
+        cy.get('ul li').contains(`${firstUser.firstName}`).find('.edit').click()
+
+        // then
+        cy.get('[name=username]').should('have.value', firstUser.username)
+        cy.get('[name=firstName]').should('have.value', firstUser.firstName)
+        cy.get('[name=lastName]').should('have.value', firstUser.lastName)
+        cy.get('[name=email]').should('have.value', firstUser.email)
+        cy.get('[name=roles]').should('have.value', firstUser.roles.join(','))
+    })
+
+    it('should update user', () => {
+        // given
+        cy.get('ul li').contains(`${firstUser.firstName}`).find('.edit').click()
+
+        const newFirstName = getRandomString()
+        const newLastName = getRandomString()
+        const newEmail = getRandomEmail()
+        cy.get('[name=email]').clear().type(newEmail)
+        cy.get('[name=firstName]').clear().type(newFirstName)
+        cy.get('[name=lastName]').clear().type(newLastName)
+
+        cy.intercept('PUT', `**/users/${firstUser.username}`, { statusCode: 200 }).as('putRequest')
+
+        // when
+        cy.get('.btn-primary').click()
+
+        // then
+        cy.wait('@putRequest').its('request.body').should('deep.equal', {
+            username: firstUser.username,
+            firstName: newFirstName,
+            lastName: newLastName,
+            roles: firstUser.roles,
+            email: newEmail
+        })
+
+    })
+
+
+})
