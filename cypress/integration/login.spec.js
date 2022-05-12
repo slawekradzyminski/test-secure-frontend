f74d1c6137c9c37e85ce13e6953049531300b037
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-05-12 21:53:22 +0200

    men12

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -4,7 +4,7 @@ describe('login page', () => {
     beforeEach(() => {
         cy.visit('http://localhost:8081')
     })
-  
+
     it('should successfully login', () => {
         cy.get('[name=username]').type('admin')
         cy.get('[name=password]').type('admin')
@@ -12,5 +12,31 @@ describe('login page', () => {
 
         cy.get('h1').should('contain.text', 'Slawomir')
     })
-  })
-  
+
+    it('should fail to login', () => {
+        cy.get('[name=username]').type('wrong')
+        cy.get('[name=password]').type('wrong')
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert')
+            .should('have.text', 'Invalid username/password supplied')
+            .and('have.class', 'alert-danger')
+    })
+
+    it('should navigate to register page', () => {
+        cy.get('a[href="/register"]').click()
+
+        cy.url().should('contain', '/register')
+        cy.get('h2').should('contain.text', 'Register')
+    })
+
+    it.only('should trigger frontend validation', () => {
+        cy.get('.btn-primary').click()
+        cy.get('.invalid-feedback').should('have.length', 2)
+        cy.get('.invalid-feedback').eq(0).should('have.text', 'Required field length is 4 or more')
+        cy.get('.invalid-feedback').eq(1).should('have.text', 'Required field length')
+        cy.get('[name=username]').should('have.class', 'is-invalid')
+        cy.get('[name=password]').should('have.class', 'is-invalid')
+    })
+
+})
