943bb2003cd1018c0fe657cac1b1eda85295c346
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-05-13 11:26:24 +0200

    for37

---
+++ cypress/integration/isolation/edit.isolated.spec.ts
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+import { Roles } from "../../util/roles"
+import users from "../../fixtures/users.json"
+
+describe('home page with mocks', () => {
+    const editedUser = users[0]
+
+    beforeEach(() => {
+        const user = {
+            roles: [Roles.ROLE_ADMIN]
+        }
+        localStorage.setItem('user', JSON.stringify(user))
+        cy.intercept('GET', '**/users', { body: users })
+        cy.visit('http://localhost:8081')
+        cy.viewport('macbook-16')
+        cy.get('ul li').contains(`${editedUser.firstName}`).find('.edit').click()
+    })
+
+    it('should autofill all date', () => {
+        cy.get('[name=firstName]').should('have.value', editedUser.firstName)
+        cy.get('[name=lastName]').should('have.value', editedUser.lastName)
+        cy.get('[name=email]').should('have.value', editedUser.email)
+        cy.get('[name=username]').should('have.value', editedUser.username)
+        cy.get('[name=roles]').should('have.value', editedUser.roles.join(','))
+    })
+
+})
