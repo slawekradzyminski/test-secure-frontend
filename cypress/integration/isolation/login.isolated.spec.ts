51b10c17eed0d40094f901f6406c95d8001fb9ac
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-05-14 15:54:21 +0200

    men213

--- cypress/integration/isolation/login.isolated.spec.ts
+++ cypress/integration/isolation/login.isolated.spec.ts
@@ -30,4 +30,28 @@ describe('login page', () => {
         cy.get('h1').should('contain.text', user.firstName)
     })
 
+    it('should fail to login', () => {
+        const message = 'Invalid username/password supplied'
+
+        cy.intercept('POST', '**/users/signin', {
+            statusCode: 422,
+            body: {
+                error: "Unprocessable Entity",
+                message: message,
+                path: "/users/signin",
+                status: 422,
+                timestamp: "2022-05-14T13:46:33.767+00:00"
+            }
+        })
+
+        cy.get('[name=username]').type('wrong')
+        cy.get('[name=password]').type('wrong')
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert')
+            .should('have.text', message)
+            .and('have.class', 'alert-danger')
+
+    })
+
 })
