284f6a0ee69e6ef87959be8f858e967b1781de11
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-05-28 12:55:57 +0200

    cz13

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -7,6 +7,19 @@ describe('example to-do app', () => {
         cy.visit('http://localhost:8081')
     })
 
+    it('should show loading indicator', () => {
+        const user = getRandomUser()
+        cy.intercept('POST', '**/users/signin', {
+            delay: 2000
+        })
+
+        cy.get('[name=username]').type(user.username)
+        cy.get('[name=password]').type(user.password)
+        cy.get('.btn-primary').click()
+
+        cy.get('.btn-primary .spinner-border').should('be.visible')
+    })
+
     it('should successfully login', () => {
         const user = getRandomUser()
 
@@ -31,9 +44,35 @@ describe('example to-do app', () => {
         cy.get('h1').should('contain.text', user.firstName)
     })
 
+    it.only('should successfully login from mobile', () => {
+        const user = getRandomUser()
+        cy.viewport('iphone-8')
+
+        cy.intercept('POST', '**/users/signin', {
+            statusCode: 200,
+            body: {
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                roles: user.roles,
+                token: "fakeToken",
+                username: user.username
+            },
+            throttleKbps: 1000
+        })
+
+        cy.intercept('GET', '**/users', { fixture: 'users.json' })
+
+        cy.get('[name=username]').type(user.username)
+        cy.get('[name=password]').type(user.password)
+        cy.get('.btn-primary').click()
+
+        cy.get('h1').should('contain.text', user.firstName)
+    })
+
     it('should fail to login', () => {
         const user = getRandomUser()
-        cy.intercept('POST', '**/users/login', {
+        cy.intercept('POST', '**/users/signin', {
             statusCode: 422,
             fixture: 'wrongLogin.json'
         })
@@ -43,6 +82,9 @@ describe('example to-do app', () => {
         cy.get('.btn-primary').click()
 
         cy.get('.alert').should('contain.text', 'Invalid username/password')
+            .and('have.class', 'alert-danger')
     })
+
+
 
 })
