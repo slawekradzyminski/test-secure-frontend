2f5016282c4d3294f05cbca4c9cc4ef5169eb969
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-06-11 12:57:33 +0200

    june17

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,10 +3,17 @@
 import { getRandomEmail, getRandomString } from "../util/random"
 
 describe('login page', () => {
+    const username = getRandomString()
+    const password = getRandomString()
+    const firstName = getRandomString()
+    const lastName = getRandomString()
+    const email = getRandomEmail()
+
+    before(() => {
+        cy.register(username, password, firstName, lastName, email)
+    })
+
     beforeEach(() => {
-        const username = getRandomString()
-        const password = getRandomString()
-        cy.register(username, password, getRandomString(), getRandomString(), getRandomEmail())
         cy.login(username, password)
         cy.visit('http://localhost:8081')
     })
@@ -25,5 +32,10 @@ describe('login page', () => {
         cy.get('#addmore').click()
         cy.url().should('contain', '/add-user')
         cy.get('h2').should('contain.text', 'Register')
+    })
+
+    it.only('should open edit user', () => {
+        cy.get('ul li').contains(firstName).find('.edit').click()
+        cy.get('h2').should('have.text', 'Edit user')
     })
 })
