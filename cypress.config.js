46a7674d6f9098070d4149b871ca1b14580416c5
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-06-11 15:42:09 +0200

    coverage report

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    video: false,
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config)
       return config
