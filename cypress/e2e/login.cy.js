57c28462daf17cb034f65f648bfe6ff3c620770b
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-06-11 08:38:41 +0200

    june12

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -11,6 +11,26 @@ describe('login page', () => {
 
         cy.get('h1').should('contain.text', 'Slawomir')
     })
+
+    it('should open register page', () => {
+        cy.get('.btn-link').click()
+        cy.get('h2').should('contain.text', 'Register')
+    })
+
+    it('should fail to login', () => {
+        cy.get('[name=username]').type('wrong')
+        cy.get('[name=password]').type('wrong')
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert').should('contain.text', 'Invalid username/password')
+    })
+
+    it('should trigger frontend validation', () => {
+        cy.get('.btn-primary').click()
+
+        cy.get('.invalid-feedback').eq(0).should('contain.text', 'Required field length')
+        cy.get('.invalid-feedback').eq(1).should('contain.text', 'Required field length')
+    })
   
   })
   
