ac612cf8f9eee0f5becbc99e271a23ae4639ed63
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-06-12 13:50:27 +0200

    fix

--- cypress/component/HomePage.cy.js
+++ cypress/component/HomePage.cy.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {HomePage} from '../../src/components/HomePage';
 
 describe('HomePage', () => {
-  it('display all users', () => {
+  beforeEach(() => {
     const user = {
       token: 'fakeToken',
       roles: ['ROLE_ADMIN']
     }
     localStorage.setItem('user', JSON.stringify(user))
     cy.intercept('GET', '**/users', {fixture: 'users.json'})
+  })
+
+
+  it('display all users', () => {
     cy.mount(<HomePage/>, '/')
     cy.get('ul li').should('have.length', 3)
   })
