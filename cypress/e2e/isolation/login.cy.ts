91620ba07dadc2b2ab87100520c4d118d603fdd7
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-06-12 08:14:09 +0200

    june21

---
+++ cypress/e2e/isolation/login.cy.ts
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+import { loginRequestAlias, mockFailedLogin, mockSuccessfulLogin } from "../../mocks/loginMocks"
+import LoginPage from "../../pages/LoginPage"
+import { getAliasedRequest } from "../../util/alias"
+import { getRandomUser } from "../../util/user"
+
+const loginPage = new LoginPage()
+
+describe('login page in isolation', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:8081')
+    })
+
+    it('should successfully login', () => {
+        // given
+        const user = getRandomUser()
+        mockSuccessfulLogin(user)
+
+        // when
+        loginPage.attemptLogin(user.username, user.password)
+
+        // then
+        cy.get('h1').should('contain.text', user.firstName)
+        cy.wait(getAliasedRequest(loginRequestAlias)).its('request.body').should('deep.equal', {
+            username: user.username,
+            password: user.password
+        })
+    })
+
+    it('should fail to login', () => {
+        // given
+        const message = "Invalid username/password supplied"
+        mockFailedLogin(message)
+
+        // when
+        loginPage.attemptLogin('wrong', 'wrong')
+
+        // then
+        cy.get('.alert').should('contain.text', message)
+    })
+
+})
---
+++ cypress/mocks/loginMocks.ts
@@ -0,0 +1,32 @@
+import { HttpMethod } from "../util/httpMethods";
+import { User } from "../util/user";
+
+const loginUrl = '**/users/signin'
+export const loginRequestAlias = 'loginRequest'
+
+export const mockSuccessfulLogin = (user: User) => {
+    cy.intercept(HttpMethod.POST, loginUrl, {
+        statusCode: 200,
+        body: {
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            roles: user.roles,
+            token: "fakeToken",
+            username: user.username
+        }
+    }).as(loginRequestAlias)
+}
+
+export const mockFailedLogin = (message: string) => {
+    cy.intercept(HttpMethod.POST, loginUrl, {
+        statusCode: 422,
+        body: {
+            error: "Unprocessable Entity",
+            message: message,
+            path: "/users/signin",
+            status: 422,
+            timestamp: "2022-06-12T05:57:11.895+00:00"
+        }
+    })
+}
