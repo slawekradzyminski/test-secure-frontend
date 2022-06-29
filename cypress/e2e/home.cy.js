cd9f9dde83b552f9627f59cc7d46167a752c8a3e
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-06-29 16:56:55 +0200

    app06

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -10,4 +10,14 @@ describe('login page', () => {
         cy.get('ul li').should('have.length.at.least', 1)
     })
 
+    it('should logout', () => {
+        cy.get('#logout').click()
+        cy.url().should('contain', '/login')
+    })
+
+    it('should redirect to adduser page', () => {
+        cy.get('#addmore').click()
+        cy.url().should('contain', '/add-user')
+    })
+
 })
