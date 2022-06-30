927a42afa79e3ce7281ff09abc6723a053a19b0a
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-06-30 16:05:39 +0200

    app16

--- cypress/e2e/isolation/login.isolated.cy.ts
+++ cypress/e2e/isolation/login.isolated.cy.ts
@@ -32,7 +32,42 @@ describe('login page is isolation', () => {
         cy.get('h1').should('contain.text', user.firstName)
     })
 
-    
+    it('should fail to login', () => {
+        // given
+        cy.intercept('POST', '**/users/signin', {
+            statusCode: 422,
+            body: {
+                error: "Unprocessable Entity",
+                message: "Invalid username/password supplied",
+                path: "/users/signin",
+                status: 422,
+                timestamp: "2022-06-30T13:48:09.648+00:00"
+            }
+        })
+ 
+        // when
+        cy.getById('username').type('wrong')
+        cy.getById('password').type('wrong')
+        cy.get('.btn-primary').click()
 
+        // then
+        cy.get('.alert').should('contain.text', 'Invalid username/password')
+            .and('have.class', 'alert-danger')
+    })
+
+    it('should handle network error', () => {
+        // given
+        cy.intercept('POST', '**/users/signin', {
+            delay: 2000
+        })
+ 
+        // when
+        cy.getById('username').type('wrong')
+        cy.getById('password').type('wrong')
+        cy.get('.btn-primary').click()
+
+        // then
+        cy.get('.btn-primary .spinner-border').should('be.visible')
+    })
 
 })
