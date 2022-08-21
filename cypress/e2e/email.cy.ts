3cd547f393f63c7d13b8e31d957ce187eeea3e31
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-08-21 16:16:49 +0200

    august05

--- cypress/e2e/email.cy.ts
+++ cypress/e2e/email.cy.ts
@@ -2,6 +2,7 @@
 
 import { getRandomString } from "../util/random"
 import { EmailMessage } from "../email/mailhogTypes"
+import { recurse } from 'cypress-recurse'
 
 describe('example to-do app', () => {
     beforeEach(() => {
@@ -22,24 +23,37 @@ describe('example to-do app', () => {
         cy.get('[name=message]').type(emailBody)
         cy.get('.btn-primary').click()
 
+        cy.intercept('GET', '**/api/v2/messages', {
+            statusCode: 200,
+            body: {}
+        })
+
         // then
         cy.get('.alert-success').should('contain.text', 'Email was scheduled to be send')
-        cy.request({
-            method: 'GET',
-            url: 'http://localhost:8025/api/v2/messages',
-        }).then(resp => {
-            expect(resp.status).to.eq(200)
-            const mailhogResponse = resp.body as EmailMessage
-            expect(mailhogResponse.count).to.be.greaterThan(0)
+        recurse(
+            () => cy.request({ url: 'http://localhost:8025/api/v2/messages' }),
+            (resp) => {
+                expect(resp.status).to.eq(200)
+                const mailhogResponse = resp.body as EmailMessage
+                expect(mailhogResponse.count).to.be.greaterThan(0)
 
-            const allSubjects = mailhogResponse.items
-                .map(item => item.Content.Headers.Subject)
-            expect(JSON.stringify(allSubjects)).to.contain(emailSubject)
+                const allSubjects = mailhogResponse.items
+                    .map(item => item.Content.Headers.Subject)
+                    .flat()
 
-            const allBodies = mailhogResponse.items
-            .map(item => item.Content.Body)
-            expect(JSON.stringify(allBodies)).to.contain(emailBody)
-        })
+                const allBodies = mailhogResponse.items
+                    .map(item => item.Content.Body)
+                    .flat()
+
+                return allSubjects.includes("Fake string") && allBodies.includes("Fake string")
+            },
+            {
+                log: true,
+                limit: 50, // max number of iterations
+                timeout: 30000, // time limit in ms
+                delay: 500, // delay before next iteration, ms
+              }
+        )
     })
 
 
