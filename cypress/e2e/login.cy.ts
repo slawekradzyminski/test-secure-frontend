0b45d374f405c8e0e9e8a8738bb8a2ef36e8e4ab
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-09-12 12:25:19 +0200

    samsung03

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -5,20 +5,28 @@ import { getRandomUser } from "../util/userProvider"
 describe('Login tests', () => {
 
     const testUser = getRandomUser()
+    let token: string
 
     beforeEach(() => {
         cy.visit('http://localhost:8081')
-        cy.request('POST', 'http://localhost:4001/users/signup', {
-            username: testUser.username,
-            firstName: testUser.firstName,
-            lastName: testUser.lastName,
-            password: testUser.password,
-            roles: testUser.roles,
-            email: testUser.email
-        }).then(resp => {
-            expect(resp.status).to.eq(201)
+        cy.request('POST', 'http://localhost:4001/users/signup', testUser)
+            .then(resp => {
+                expect(resp.status).to.eq(201)
+                token = resp.body.token
+            })
+    })
+
+    afterEach(() => {
+        cy.request({
+            method: 'DELETE',
+            url: `http://localhost:4001/users/${testUser.username}`,
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
         })
-        
+            .then(resp => {
+                expect(resp.status).to.eq(204)
+            })
     })
 
     it('should successfully login', () => {
