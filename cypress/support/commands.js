6ccde3b158a3d3ddf32529910a2bee52746156c8
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-09-17 10:41:42 +0200

    ich08

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,18 @@
 Cypress.Commands.add('login', (username, password) => { 
-    cy.get('input[name=username]').type(username)
-    cy.get('input[name=password]').type(password)
-    cy.get('.btn-primary').click()
+    // 1 - Najpierw musimy wysłać request o logowanie
+    // 2 - Jak przyjdzie odpowiedź to musimy ją ustawić w localStorage
+
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:4001/users/signin', 
+        body: {
+          username: username,
+          password: password,
+        },
+      }).then(resp => {
+        expect(resp.status).to.eq(200)
+        localStorage.setItem('user', JSON.stringify(resp.body))
+      })
  })
 
 //  Cypress.Commands.add('login', (username, password) => { 
