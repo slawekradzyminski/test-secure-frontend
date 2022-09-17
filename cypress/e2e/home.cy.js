78715a4892132da1002b7d40d18f29d47f866325
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-09-17 09:50:04 +0200

    ich07

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,9 +3,7 @@
 describe('Home page tests', () => {
     beforeEach(() => {
         cy.visit('http://localhost:8081')
-        cy.get('input[name=username]').type('admin')
-        cy.get('input[name=password]').type('admin')
-        cy.get('.btn-primary').click()
+        cy.login('admin', 'admin')
     })
 
     it('should display at least one user', () => {
