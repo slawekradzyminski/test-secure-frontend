279ddc065dd96cd0d15d4e2f14497ea1e15fe0bb
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-09-18 14:41:42 +0200

    ich31

--- cypress/e2e/isolation/home.isolated.cy.ts
+++ cypress/e2e/isolation/home.isolated.cy.ts
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import MockGetUsers from "../../stubs/MockGetUsers"
+import users from "../../fixtures/users.json"
 
 describe('Home page isolated tests', () => {
     beforeEach(() => {
@@ -10,6 +11,10 @@ describe('Home page isolated tests', () => {
     })
 
     it('should display all users', () => {
-
+        // then
+        cy.get('li').should('have.length', users.length)
+        cy.get('li').each(($el, i) => {
+            expect($el.text()).to.contain(`${users[i].firstName} ${users[i].lastName}`)
+        })
     })
 })
