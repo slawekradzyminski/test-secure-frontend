79ea8f9d1d85a48f8b1a62140865faa1eae35952
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-10-05 10:31:25 +0200

    teb08

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -2,28 +2,7 @@
 
 describe('Home page tests', () => {
     beforeEach(() => {
-        // 1. Mam wysłać request na /users/signin z poprawnymi danymi logowania
-        // 2. Oczekuję odpowiedzi http 200 bo to znaczy ze logowanie się udało
-        // 3. Chcę ustawić odpowiedź w localStorage
-        // 4. Chcę ustawić token z odpowiedzi w ciastku o nazwie token
-
-        // 1.
-        cy.request({
-            method: 'POST',
-            url: 'http://localhost:4001/users/signin',
-            body: {
-                username: 'admin',
-                password: 'admin',
-            }
-        }).then((resp) => {
-            // 2.
-            expect(resp.status).to.eq(200)
-            // 3.
-            localStorage.setItem('user', JSON.stringify(resp.body))
-            // 4.
-            cy.setCookie('token', resp.body.token)
-        })
-
+        cy.login('admin', 'admin')
         cy.visit('')
         cy.getCookie('token').its('value').should('not.be.empty')
     })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,25 +1,20 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (username, password) => {
+    // 1. Mam wysłać request na /users/signin z poprawnymi danymi logowania
+    // 2. Oczekuję odpowiedzi http 200 bo to znaczy ze logowanie się udało
+    // 3. Chcę ustawić odpowiedź w localStorage
+    // 4. Chcę ustawić token z odpowiedzi w ciastku o nazwie token
+
+    // 1.
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:4001/users/signin',
+        body: { username, password }
+    }).then((resp) => {
+        // 2.
+        expect(resp.status).to.eq(200)
+        // 3.
+        localStorage.setItem('user', JSON.stringify(resp.body))
+        // 4.
+        cy.setCookie('token', resp.body.token)
+    })
+})
