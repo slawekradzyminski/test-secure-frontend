3100e27bcddb7cbb74e102ec7f281c03e146b2ab
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-10-05 15:32:12 +0200

    teb20

--- cypress/e2e/edit.cy.ts
+++ cypress/e2e/edit.cy.ts
@@ -3,14 +3,22 @@
 import { getRandomUser, User } from "../domain/user"
 
 describe('Edit page tests', () => {
+    let token: string
     let user: User
 
     beforeEach(() => {
         user = getRandomUser()
         cy.register(user)
-        cy.login(user.username, user.password)
+        cy.login(user.username, user.password).then((returnedToken) => {
+            cy.setCookie('token', returnedToken)
+            token = returnedToken
+        })
         cy.visit('')
         cy.get('li').contains(`${user.firstName} ${user.lastName}`).find('.edit').click()
+    })
+
+    afterEach(() => {
+        cy.deleteUser(user.username, token)
     })
 
     it('should correctly autofill data', () => {
@@ -21,16 +29,29 @@ describe('Edit page tests', () => {
         cy.get('[name=roles]').should('have.value', user.roles.join(','))
     })
 
-    it('should edit user', () => {
+    it.only('should edit user', () => {
         const newUser = getRandomUser()
         cy.get('[name=firstName]').clear().type(newUser.firstName)
         cy.get('[name=lastName]').clear().type(newUser.lastName)
-        cy.get('[name=email]').clear().type(newUser.email)
         cy.get('[name=email]').clear().type(newUser.email)
         cy.get('.btn-primary').click()
 
         cy.get('li').contains(`${newUser.firstName} ${newUser.lastName}`).should('exist')
         cy.get('li').contains(`${user.firstName} ${user.lastName}`).should('not.exist')
+        cy.get('.alert-success').should('have.text', 'Updating user successful')
+
+        cy.request({
+            method: 'GET',
+            url: `http://localhost:4001/users/${user.username}`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((resp) => {
+            expect(resp.status).to.eq(200)
+            expect(resp.body.firstName).to.eq(newUser.firstName)
+            expect(resp.body.lastName).to.eq(newUser.lastName)
+            expect(resp.body.email).to.eq(newUser.email)
+        })
     })
 
 })
