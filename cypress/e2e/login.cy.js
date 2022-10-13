9b719178b667f5edcdbd6651bf26fa144e5f330d
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-10-13 21:52:46 +0200

    rower02

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,12 +5,28 @@ describe('Login page tests', () => {
         cy.visit('http://localhost:8081')
     })
 
-    it('should successfully', () => {
+    it('should successfully login', () => {
         cy.get('input[name=username]').type('admin')
         cy.get('input[name=password]').type('admin')
         cy.get('.btn-primary').click()
 
         cy.get('h1').should('contain.text', 'Slawomir')
+    })
+
+    it('should fail to login', () => {
+        cy.get('input[name=username]').type('wrong')
+        cy.get('input[name=password]').type('credentials')
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert').should('have.text', 'Invalid username/password supplied')
+        cy.url().should('contain', '/login')
+    })
+    
+    it('should open register page', () => {
+        cy.get('.btn-link').click()
+
+        cy.get('h2').should('contain.text', 'Register')
+        cy.url().should('contain', '/register')
     })
 
 })
