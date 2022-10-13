1d731e85d2c1e4dd5f1718f2b0bbaccc52da4a85
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-10-13 21:37:21 +0200

    rower01

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe('Login page tests', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:8081')
+    })
+
+    it('should successfully', () => {
+        cy.get('input[name=username]').type('admin')
+        cy.get('input[name=password]').type('admin')
+        cy.get('.btn-primary').click()
+
+        cy.get('h1').should('contain.text', 'Slawomir')
+    })
+
+})
