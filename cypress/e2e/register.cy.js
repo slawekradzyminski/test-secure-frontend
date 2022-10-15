bb489cd517f5e26be921effc39de18dd112ce134
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-10-15 11:55:47 +0200

    rower07

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import { getRandomEmail, getRandomString } from "../util/random"
+import { faker } from '@faker-js/faker';
 
 describe('Register page tests', () => {
     beforeEach(() => {
@@ -8,11 +9,11 @@ describe('Register page tests', () => {
     })
 
     it('should successfully register', () => {
-        cy.get('[name=firstName]').type(getRandomString())
-        cy.get('[name=lastName]').type(getRandomString())
-        cy.get('[name=username]').type(getRandomString())
-        cy.get('[name=password]').type(getRandomString())
-        cy.get('[name=email]').type(getRandomEmail())
+        cy.get('[name=firstName]').type(faker.name.firstName())
+        cy.get('[name=lastName]').type(faker.name.lastName())
+        cy.get('[name=username]').type(faker.internet.userName())
+        cy.get('[name=password]').type(faker.internet.password())
+        cy.get('[name=email]').type(faker.internet.email())
         cy.get('.btn-primary').click()
 
         cy.get('.alert').should('contain.text', 'Registration successful')
