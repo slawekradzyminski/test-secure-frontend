a424ef315cb6be7206e79d593fe6eaedc21258c9
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-10-15 09:59:24 +0200

    rower04

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -29,4 +29,18 @@ describe('Login page tests', () => {
         cy.url().should('contain', '/register')
     })
 
+    it('should trigger frontend validation', () => {
+        cy.get('.btn-primary').click()
+
+        cy.get('.invalid-feedback').should('have.length', 2)
+        cy.get('.invalid-feedback').each(($el) => {
+            cy.wrap($el).should('have.text', 'Required field length is 4 or more')
+        })
+
+        cy.get('.is-invalid').should('have.length', 2)
+        cy.get('input').each(($el) => {
+            cy.wrap($el).should('have.class', 'is-invalid')
+        })
+    })
+
 })
--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -31,4 +31,19 @@ describe('Register page tests', () => {
         cy.url().should('contain', '/register')
     })
 
+    it('should trigger frontend validation', () => {
+        cy.get('.btn-primary').click()
+        const numberOfRows = 5
+
+        cy.get('.invalid-feedback').should('have.length', numberOfRows)
+        cy.get('.invalid-feedback').each(($el) => {
+            cy.wrap($el).should('have.text', 'Required field length is 4 or more')
+        })
+
+        cy.get('.is-invalid').should('have.length', numberOfRows)
+        cy.get('input').each(($el) => {
+            cy.wrap($el).should('have.class', 'is-invalid')
+        })
+    })
+
 })
