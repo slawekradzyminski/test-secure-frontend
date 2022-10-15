dea100109895c4195d6388aa1d47539d47f1efa4
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-10-15 15:19:35 +0200

    rower15

---
+++ cypress/e2e/edit.cy.ts
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+import { getRandomUser, User } from "../domain/user"
+
+describe('Edit page tests', () => {
+    let user: User
+
+    beforeEach(() => {
+        user = getRandomUser()
+        cy.register(user)
+        cy.login(user.username, user.password)
+        cy.visit('')
+        cy.get('li').contains(`${user.firstName} ${user.lastName}`).find('.edit').click()
+    })
+
+    it('should correctly autofill user data', () => {
+        cy.get('[name=firstName]').should('have.value', user.firstName)
+        cy.get('[name=lastName]').should('have.value', user.lastName)
+        cy.get('[name=username]').should('have.value', user.username)
+        cy.get('[name=email]').should('have.value', user.email)
+        cy.get('[name=roles]').should('have.value', user.roles.join(','))
+    })
+
+})
