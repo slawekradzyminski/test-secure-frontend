cb4efb93cdbd2eb0af152f1ed9e836db4dbf8433
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-10-16 15:41:42 +0200

    rower36

--- cypress/e2e/edit.cy.ts
+++ cypress/e2e/edit.cy.ts
@@ -28,11 +28,8 @@ describe('Edit page tests', () => {
 
     it('should correctly edit an user', () => {
         const newUser = getRandomUser()
-        cy.get('[name=firstName]').clear().type(newUser.firstName)
-        cy.get('[name=lastName]').clear().type(newUser.lastName)
-        cy.get('[name=email]').clear().type(newUser.email)
-        cy.get('.btn-primary').click()
-
+        editPage.fillAvailableInputs(newUser)
+        
         cy.get('li').contains(`${user.firstName} ${user.lastName}`).should('not.exist')
         cy.get('li').contains(`${newUser.firstName} ${newUser.lastName}`).should('be.visible')
 
--- cypress/pages/editPage.ts
+++ cypress/pages/editPage.ts
@@ -6,7 +6,8 @@ export const editPage = {
         lastNameInput: '[name=lastName]',
         usernameInput: '[name=username]',
         emailInput: '[name=email]', 
-        rolesInput: '[name=roles]'
+        rolesInput: '[name=roles]',
+        editButton: '.btn-primary'
     },
 
     checkDataInEditFields: (user: User) => {
@@ -15,5 +16,12 @@ export const editPage = {
         cy.get(editPage.selectors.usernameInput).should('have.value', user.username)
         cy.get(editPage.selectors.emailInput).should('have.value', user.email)
         cy.get(editPage.selectors.rolesInput).should('have.value', user.roles.join(','))
+    },
+
+    fillAvailableInputs: (user: User) => {
+        cy.get(editPage.selectors.firstNameInput).clear().type(user.firstName)
+        cy.get(editPage.selectors.lastNameInput).clear().type(user.lastName)
+        cy.get(editPage.selectors.emailInput).clear().type(user.email)
+        cy.get(editPage.selectors.editButton).click()
     }
 }
