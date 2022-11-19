ad8b52147e96913da6f9e15acab809d0b4b98b33
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-11-19 12:21:14 +0100

    move02

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
   },
   e2e: {
     baseUrl: 'http://localhost:8081',
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
