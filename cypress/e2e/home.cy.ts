52ec659368c89e5f13b6b1610de36821fdc18866
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-11-19 14:45:39 +0100

    move06

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -7,8 +7,8 @@ describe('home page tests', () => {
     beforeEach(() => {
         user = getRandomUser()
         cy.register(user)
-        cy.login(user.username, user.password).then(returnedToken => { 
-            token = returnedToken 
+        cy.login(user.username, user.password).then(returnedToken => {
+            token = returnedToken
             cy.setCookie('token', returnedToken)
         })
         cy.visit('/')
@@ -30,6 +30,24 @@ describe('home page tests', () => {
     it('should open add more page', () => {
         cy.get('#addmore').click()
         cy.url().should('contain', 'add-user')
+    })
+
+    it('should delete all users except the current one', () => {
+        cy.get('ul li').each(($el) => {
+            if (!$el.text().includes(`${user.firstName} ${user.lastName}`)) {
+                cy.wrap($el).find('.delete').click()
+            }
+        })
+    })
+
+    it('should cancel user deletion', () => {
+        Cypress.on('window:confirm', (confirmationText) => {
+            expect(confirmationText).to.eq('Are you sure you wish to delete this item?')
+            return false
+        })
+
+        cy.get('ul li').contains(`${user.firstName} ${user.lastName}`).find('.delete').click()
+        cy.get('ul li').contains(`${user.firstName} ${user.lastName}`).should('exist')
     })
 
 })
