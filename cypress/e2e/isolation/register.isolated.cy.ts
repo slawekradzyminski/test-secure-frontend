ffdce89c7fe6a96e05d87ebdcdc1af4dd58700af
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-12-11 12:51:07 +0100

    kufel21

---
+++ cypress/e2e/isolation/register.isolated.cy.ts
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+import { getRandomUser } from '../../utils/user';
+
+describe('Register page tests', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:8081/register')
+    })
+
+    it.only('should successfully register', () => {
+        const user = getRandomUser()
+
+        cy.intercept('POST', '**/users/signup', {
+            statusCode: 201,
+            body: {
+                token: 'fakeToken'
+            }
+        })
+
+        cy.get('[name=firstName]').type(user.firstName)
+        cy.get('[name=lastName]').type(user.lastName)
+        cy.get('[name=username]').type(user.username)
+        cy.get('[name=password]').type(user.password)
+        cy.get('[name=email]').type(user.email)
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert-success').should('contain.text', 'Registration successful')
+
+    })
+
+})
+
