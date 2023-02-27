4aafa8b5e3a1acd5f2c3ce5d90fcd7fe0e5bb252
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-02-27 15:55:07 +0100

    stone07

---
+++ cypress/e2e/testarena.cy.js
@@ -0,0 +1,21 @@
+import { faker } from '@faker-js/faker';
+
+/// <reference types="cypress" />
+
+describe('Testarena training', () => {
+    beforeEach(() => {
+        cy.visit('http://demo.testarena.pl/')
+        cy.get('#email').type('administrator@testarena.pl')
+        cy.get('#password').type('sumXQQ72$L')
+        cy.get('#login').click()
+    })
+
+    it('should add new message', () => {
+        cy.get('.icon_mail').click()
+        const message = faker.random.alphaNumeric(10)
+        cy.get('#j_msgContent').type(message)
+        cy.get('#j_msgResponse-193').click()
+        cy.get('.message_content_text').should('contain', message)
+    })
+
+})
