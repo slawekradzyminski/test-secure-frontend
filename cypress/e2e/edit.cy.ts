ae9f5f67d2c988b1ecfaa2fb496baa3baef5fc6f
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-02-28 13:29:19 +0100

    stone14

---
+++ cypress/e2e/edit.cy.ts
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+import { getRandomUser, User } from "../domain/user"
+
+
+describe('Edit page tests', () => {
+    let user: User
+
+    beforeEach(() => {
+        user = getRandomUser()
+        cy.register(user)
+        cy.login(user.username, user.password)
+        cy.visit('http://localhost:8081')
+        cy.contains('li', `${user.firstName} ${user.lastName}`).find('.edit').click()
+    })
+
+    it('should correctly autofill data', () => {
+        // then
+        cy.get('[name=firstName]').should('have.value', user.firstName)
+        cy.get('[name=lastName]').should('have.value', user.lastName)
+        cy.get('[name=username]').should('have.value', user.username)
+        cy.get('[name=email]').should('have.value', user.email)
+        cy.get('[name=roles]').should('have.value', user.roles.join(', '))
+
+    })
+
+})
