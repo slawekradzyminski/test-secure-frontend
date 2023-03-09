ee92f35177a4b7e0bee61ebdaa86e07e71a53526
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-03-09 20:57:44 +0100

    wio01

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Login page tests', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:8081')
+    })
+  
+    it('should successfully login', () => {
+
+    })
+  })
+  
