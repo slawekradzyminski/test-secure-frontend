4330799521ad0fc6b290585878d0abebaf2e8356
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-03-09 21:55:08 +0100

    wio02

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,11 +2,22 @@
 
 describe('Login page tests', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:8081')
+        cy.visit('http://localhost:8081')
     })
-  
-    it('should successfully login', () => {
 
+    it('should successfully login', () => {
+        cy.get('[name=username]').type('admin')
+        cy.get('[name=password]').type('admin')
+        cy.get('.btn-primary').click()
+
+        cy.get('h1').should('contain.text', 'Slawomir')
     })
-  })
-  
+
+    it('should show error message on failed login', () => {
+        cy.get('[name=username]').type('wrong')
+        cy.get('[name=password]').type('wrong')
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert').should('contain.text', 'Invalid username/password')
+    })
+})
