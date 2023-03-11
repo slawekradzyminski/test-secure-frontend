a74f5f529ca948a328c337247ec0d0b6846f091d
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-03-11 10:35:24 +0100

    wio07

--- cypress/e2e/register.cy.ts
+++ cypress/e2e/register.cy.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-import { faker } from '@faker-js/faker';
+import { getRandomUser } from "../utils/user"
 
 describe('Register page tests', () => {
     beforeEach(() => {
@@ -17,11 +17,12 @@ describe('Register page tests', () => {
     })
 
     it('should successfully register', () => {
-        cy.get('[name=firstName]').type(faker.name.firstName())
-        cy.get('[name=lastName]').type(faker.name.lastName())
-        cy.get('[name=username]').type(faker.internet.userName())
-        cy.get('[name=password]').type(faker.internet.password())
-        cy.get('[name=email]').type(faker.internet.email())
+        const user = getRandomUser()
+        cy.get('[name=firstName]').type(user.firstName)
+        cy.get('[name=lastName]').type(user.lastName)
+        cy.get('[name=username]').type(user.username)
+        cy.get('[name=password]').type(user.password)
+        cy.get('[name=email]').type(user.email)
         cy.get('.btn-primary').click()
 
         cy.get('.alert').should('have.text', 'Registration successful')
@@ -29,11 +30,12 @@ describe('Register page tests', () => {
     })
 
     it('should fail to register', () => {
-        cy.get('[name=firstName]').type(faker.name.firstName())
-        cy.get('[name=lastName]').type(faker.name.lastName())
+        const user = getRandomUser()
+        cy.get('[name=firstName]').type(user.firstName)
+        cy.get('[name=lastName]').type(user.lastName)
         cy.get('[name=username]').type('admin')
-        cy.get('[name=password]').type(faker.internet.password())
-        cy.get('[name=email]').type(faker.internet.email())
+        cy.get('[name=password]').type(user.password)
+        cy.get('[name=email]').type(user.email)
         cy.get('.btn-primary').click()
 
         cy.get('.alert').should('have.text', 'Username is already in use')
