40f1f881ee0852682d876994e3f49b94bf69954e
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-03-11 09:33:28 +0100

    wio06

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -16,7 +16,7 @@ describe('Register page tests', () => {
         cy.url().should('contain', '/login')
     })
 
-    it.only('should successfully register', () => {
+    it('should successfully register', () => {
         cy.get('[name=firstName]').type(faker.name.firstName())
         cy.get('[name=lastName]').type(faker.name.lastName())
         cy.get('[name=username]').type(faker.internet.userName())
@@ -26,6 +26,17 @@ describe('Register page tests', () => {
 
         cy.get('.alert').should('have.text', 'Registration successful')
         cy.url().should('contain', '/login')
+    })
+
+    it.only('should fail to register', () => {
+        cy.get('[name=firstName]').type(faker.name.firstName())
+        cy.get('[name=lastName]').type(faker.name.lastName())
+        cy.get('[name=username]').type('admin')
+        cy.get('[name=password]').type(faker.internet.password())
+        cy.get('[name=email]').type(faker.internet.email())
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert').should('have.text', 'Username is already in use')
     })
 
 })
