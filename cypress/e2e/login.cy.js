57475d938e925b9b9f1c4db30832b0b0c94144a5
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-03-11 08:24:55 +0100

    wio03

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -20,4 +20,28 @@ describe('Login page tests', () => {
 
         cy.get('.alert').should('contain.text', 'Invalid username/password')
     })
+
+    it('should show error message on failed login', () => {
+        cy.get('[name=username]').type('wrong')
+        cy.get('[name=password]').type('wrong')
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert').should('contain.text', 'Invalid username/password')
+    })
+
+    it('should trigger frontend validatio', () => {
+        cy.get('.btn-primary').click()
+
+        cy.get('.invalid-feedback')
+            .should('have.length', 2)
+            .each(($el) => {
+                cy.wrap($el).should('have.text', 'Required field length is 4 or more')
+            })
+
+        cy.get('input')
+            .should('have.length', 2)
+            .each(($el) => {
+                cy.wrap($el).should('have.class', 'is-invalid')
+            })
+    })
 })
