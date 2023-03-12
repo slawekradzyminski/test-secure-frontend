764ad5fe5489ecef37dd3fa2032aef32b983dea3
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-03-12 14:05:14 +0100

    wio32

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    retries: 1,
-    env: {
-      url: 'http://testenv1.cantest.it'
-    },
-    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/isolation/login.isolated.cy.ts
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+import LoginPage from "../../pages/LoginPage"
+import { getRandomUser } from "../../utils/user"
+
+describe('Login page tests', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:8081')
+    })
+
+    it('should successfully login', () => {
+        // given
+        const user = getRandomUser()
+        cy.intercept('POST', '**/users/signin', {
+            statusCode: 200,
+            body: {
+                username: user.username,
+                roles: user.roles,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                token: 'fakeToken',
+                email: user.email
+            }
+        })
+
+        // when
+        LoginPage.attemptLogin(user.username, user.password)
+
+        // then
+        cy.get('h1').should('contain.text', user.firstName)
+    })
+
+})
