e2b2173646fa0ae435415ddb82b699d88a261515
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-03-12 08:24:07 +0100

    wio20

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,13 +1,27 @@
 /// <reference types="cypress" />
 
-import { getRandomUser } from "../utils/user"
+import { getRandomUser, User } from "../utils/user"
 
 describe('Home page tests', () => {
+    let user: User
+    let token: string | undefined
+
     beforeEach(() => {
-        const user = getRandomUser()
+        user = getRandomUser()
         cy.register(user)
         cy.login(user.username, user.password)
         cy.visit('http://localhost:8081')
+        cy.getCookie('token').then((cookie) => token = cookie?.value)
+    })
+
+    afterEach(() => {
+        cy.request({
+            method: 'DELETE',
+            url: `http://localhost:4001/users/${user.username}`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
     })
 
     it('should display at least one user', () => {
