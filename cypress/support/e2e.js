47d3711ce4cb0f9166eca5dfc847944f28452dbe
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-04-15 11:17:20 +0200

    sky03

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,10 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+beforeEach(() => {
+    cy.log('This will run before each test in the project')
+    if (Cypress.env('isMobile')) {
+        cy.viewport('iphone-8')
+    }
+})
