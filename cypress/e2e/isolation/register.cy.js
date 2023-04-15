66655c25d8f512a44e2c701b83c178720375062a
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-04-15 12:37:51 +0200

    sky07

--- cypress/e2e/isolation/register.cy.js
+++ cypress/e2e/isolation/register.cy.js
@@ -2,11 +2,24 @@
 
 describe('Register page', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:8081')
+        cy.visit('http://localhost:8081/register')
     })
 
     it('should successfully register', () => {
+        // given
+        cy.intercept('POST', '**/signup', {
+            statusCode: 201
+        })
 
+        // when
+        cy.get('[name=username]').type('admin')
+        cy.get('[name=password]').type('admin')
+        cy.get('[name=firstName]').type('admin')
+        cy.get('[name=lastName]').type('admin')
+        cy.get('[name=email]').type('admin@cantest.it')
+        cy.get('.btn-primary').click()
 
+        // then
+        cy.url().should('contain', '/login')
     })
 })
