11b11590bd008bd38a6e14b01bcf56b84463ff21
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-04-22 11:44:44 +0200

    doge03

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,8 +1,20 @@
 /// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
 
 describe('Register tests', () => {
     beforeEach(() => {
         cy.visit('http://localhost:8081/register')
+    })
+
+    it.only('should successfully register', () => {
+        cy.get('[name=username]').type(faker.internet.userName())
+        cy.get('[name=password]').type(faker.internet.password())
+        cy.get('[name=firstName]').type(faker.name.firstName())
+        cy.get('[name=lastName]').type(faker.name.lastName())
+        cy.get('[name=email]').type(faker.internet.email())
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert').should('contain.text', 'Registration successful')
     })
 
     it('should open login page', () => {
