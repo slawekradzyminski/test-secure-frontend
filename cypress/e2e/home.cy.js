3a6632d75824cb7c7c9bb84ddcde792081a36931
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-04-23 11:42:24 +0200

    doge12

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,9 +1,22 @@
+import { getRandomUser } from "../utils/user"
+
 describe('Home page', () => {
+    // Dajemy to na poziomie całej test suity zeby mieć dostęp do tej zmiennej we wszystkich miejscach
+    // tj. w beforeEachu i testach
+    // Uzywamy leta i za kazdym razem nadpisujemy w beforeEachu zeby w kazdym teście mieć innego uzytkownika
+    let user
+
     beforeEach(() => {
+        user = getRandomUser()
+        cy.register(user)
         cy.visit('http://localhost:8081')
-        cy.get('[name=username]').type('admin')
-        cy.get('[name=password]').type('admin')
+        cy.get('[name=username]').type(user.username)
+        cy.get('[name=password]').type(user.password)
         cy.get('.btn-primary').click()
+    })
+
+    it('should be able to edit user', () => {
+        cy.get('li').contains(`${user.firstName}`).find('.edit').click()
     })
 
     it('should display at least one user', () => {
