b7c0bc05f60b7d2b596566364133d7221a76bdf9
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-05-17 11:28:55 +0200

    can04

--- cypress/e2e/testarena/testarena.home.cy.js
+++ cypress/e2e/testarena/testarena.home.cy.js
@@ -1,8 +1,10 @@
 /// <reference types="cypress" />
 describe('Home page', () => {
+
+    const email = 'administrator@testarena.pl'
+
     beforeEach(() => {
         cy.visit('http://demo.testarena.pl/zaloguj')
-        const email = 'administrator@testarena.pl'
         cy.get('#email').type(email)
         cy.get('#password').type('sumXQQ72$L')
         cy.get('#login').click()
@@ -10,9 +12,30 @@ describe('Home page', () => {
 
     it('should open my messages', () => {
         cy.get('.icon_mail').click()
-
+        
         cy.url().should('contain', 'moje_wiadomosci')
         cy.get('#j_msgSingleThreadSubject').should('be.visible')
     })
 
+    it('should open administration page', () => {
+        cy.get('.icon_tools').click()
+
+        cy.url().should('contain', 'administration')
+        cy.get('.content_title').should('have.text', 'Projekty')
+     })
+
+     it('should open user profile', () => {
+        cy.get('.top_avatar_header').click()
+
+        cy.url().should('contain', 'profil')
+        cy.get('.content_title').should('have.text', 'Profil')
+     })
+
+     it('should logout', () => {
+        cy.get('.icons-switch').click()
+
+        cy.url().should('contain', 'zaloguj')
+        cy.get('#email').should('be.visible')
+     })
+ 
 })
