4b9533d9c33f37501490a8bd3101a1e094369eef
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-05-17 15:38:59 +0200

    can11

--- cypress/e2e/email.cy.js
+++ cypress/e2e/email.cy.js
@@ -1,14 +1,24 @@
 /// <reference types="cypress" />
+
+import { getRandomEmail } from "../utils/email"
+
 describe('Email page', () => {
     beforeEach(() => {
       cy.visit('http://localhost:8081')
       cy.login('admin', 'admin')
+      cy.get('li').contains('Slawomir Radzyminski').find('.email').click()
     })
   
-    it('should successfully logout', () => {
-        cy.get('#logout').click()
-        cy.url().should('contain', 'login')
+    it.only('should successfully send email', () => {
+        const randomEmail = getRandomEmail()
+
+        cy.get('[name="subject"]').type(randomEmail.subject)
+        cy.get('[name="message"]').type(randomEmail.message)
+        cy.get('.btn-primary').click();
+
+        cy.get('.alert-success').should('have.text', 'Email was scheduled to be send')
     })
+
   
   })
   
