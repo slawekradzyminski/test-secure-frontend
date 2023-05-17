3d007de06ff341c07a4ee20dea80653e0bc2011e
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-05-17 09:46:36 +0200

    can02

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,16 +1,32 @@
 /// <reference types="cypress" />
 describe('Login page', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:8081')
-    })
-  
-    it('should successfully login', () => {
-        cy.get('[name=username]').type('admin')
-        cy.get('[name=password]').type('admin')
-        cy.get('button').contains('Login').click()
-
-        cy.get('h1').should('contain.text', 'Slawomir')
-    })
-  
+  beforeEach(() => {
+    cy.visit('http://localhost:8081')
   })
-  
+
+  it('should successfully login', () => {
+    const admin = 'admin'
+    cy.get('[name=username]').type(admin)
+    cy.get('[name=password]').type(admin)
+    cy.get('button').contains('Login').click()
+
+    cy.get('h1').should('contain.text', 'Slawomir')
+  })
+
+  it('should fail to login', () => {
+    cy.get('[name=username]').type('wrong')
+    cy.get('[name=password]').type('wrong')
+    cy.get('button').contains('Login').click()
+
+    cy.get('.alert').should('contain.text', 'Invalid username/password supplied')
+  })
+
+  it('should open Register page', () => {
+    cy.get('.btn.btn-link').click()
+    
+    // Asercje w Cypressie
+    cy.get('h2').should('have.text', 'Register')
+    cy.url().should('contain', 'register')
+  })
+
+})
