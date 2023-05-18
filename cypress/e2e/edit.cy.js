c85a1e021563df280d0065569a205e3e88355bf4
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-05-18 11:35:09 +0200

    can20

--- cypress/e2e/edit.cy.js
+++ cypress/e2e/edit.cy.js
@@ -1,18 +1,29 @@
 /// <reference types="cypress" />
 
+import { generateUser } from "../utils/user"
+
 describe('Edit page', () => {
+
+    let user
+
     beforeEach(() => {
+        user = generateUser()
+        cy.registerViaAPI(user)
+        cy.loginViaAPI(user)  
         cy.visit('http://localhost:8081')
-        cy.login('admin', 'admin')
-        cy.get('li').contains('Slawomir Radzyminski').find('.edit').click();
+        cy.get('li').contains(`${user.firstName} ${user.lastName}`).find('.edit').click()
     })
 
     it('should autofill edit profile', () => {
-        cy.get("[name='firstName']").should('have.value', 'Slawomir');
-        cy.get("[name='lastName']").should('have.value', 'Radzyminski');
-        cy.get("[name='username']").should('have.value', 'admin');
-        cy.get("[name='email']").should('have.value', 'admin@email.com');
-        cy.get("[name='roles']").should('have.value', 'ROLE_ADMIN,ROLE_CLIENT')
+        cy.get("[name='firstName']").should('have.value', user.firstName);
+        cy.get("[name='lastName']").should('have.value', user.lastName);
+        cy.get("[name='username']").should('have.value', user.username);
+        cy.get("[name='email']").should('have.value', user.email);
+        cy.get("[name='roles']").should('have.value', user.roles.join())
+    })
+
+    it('should correctly edit an user', () => {
+        
     })
 
 })
