3e9695656b14c94f6f22c076f07e98422f39e811
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-05-18 10:43:01 +0200

    can17

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -4,16 +4,40 @@ import { generateUser } from "../utils/user"
 
 describe('Home page', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:8081')
-      const user = generateUser()
-      cy.registerViaAPI(user)
-      cy.login(user.username, user.password)
+        const user = generateUser()
+        cy.registerViaAPI(user)
+
+        // 1. Musimy wysłać request logowania
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:4001/users/signin',
+            body: {
+                username: user.username,
+                password: user.password
+            }
+        }).then(resp => {
+            // 2. Odpowiedź musimy zapisać w localStorage
+            localStorage.setItem('user', JSON.stringify(resp.body))
+            // 3. Token z odpowiedzi musimy zapisać jako wartość ciastka o nazwie token
+            cy.setCookie('token', resp.body.token)
+        })
+
+        cy.visit('http://localhost:8081')
     })
-  
+
     it('should successfully logout', () => {
         cy.get('#logout').click()
         cy.url().should('contain', 'login')
     })
-  
-  })
-  
+
+    it('should display at least one user', () => {
+        cy.get('li').should('have.length.at.least', 1)
+    })
+
+    it('should open add more page', () => {
+        cy.get('#addmore').click()
+
+        cy.url().should('contain', 'add-user')
+    })
+
+})
