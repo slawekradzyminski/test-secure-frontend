45f65ef83bf86b45107034d7108c3514977baf79
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-05-18 08:54:29 +0200

    can13

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,35 @@
 /// <reference types="cypress" />
+
+import { generateRandomEmail, generateRandomString } from "../utils/random"
+
 describe('Login page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8081')
   })
 
-  it('should successfully login', () => {
-    cy.login('admin', 'admin')
+  it('should §successfully login', () => {
+    const username = generateRandomString(6)
+    const password = generateRandomString(6)
+    const firstName = generateRandomString(8)
+    const lastName = generateRandomString(8)
+    const email = generateRandomEmail(8)
 
-    cy.get('h1').should('contain.text', 'Slawomir')
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:4001/users/signup',
+      body: {
+        firstName: firstName,
+        lastName: lastName,
+        username: username,
+        password: password,
+        roles: [ "ROLE_CLIENT" ],
+        email: email
+      },
+    })
+
+    cy.login(username, password)
+
+    cy.get('h1').should('contain.text', firstName)
   })
 
   it('should fail to login', () => {
@@ -18,7 +40,7 @@ describe('Login page', () => {
 
   it('should open Register page', () => {
     cy.get('.btn.btn-link').click()
-    
+
     // Asercje w Cypressie
     cy.get('h2').should('have.text', 'Register')
     cy.url().should('contain', 'register')
