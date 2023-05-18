76fbab26f0687dab06d33974d5c23ffc0ac3794c
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-05-18 15:19:17 +0200

    can27

--- cypress/e2e/isolation/login.isolated.cy.js
+++ cypress/e2e/isolation/login.isolated.cy.js
@@ -7,7 +7,6 @@ describe('[ISOLATION] Login page', () => {
         cy.visit('http://localhost:8081')
     })
 
-
     it('should successfully login', () => {
         // given
         const user = generateUser()
@@ -30,5 +29,28 @@ describe('[ISOLATION] Login page', () => {
         // 
         cy.get('h1').should('contain.text', user.firstName)
     })
+
+    it.only('should not login', () => {
+        // given
+        const user = generateUser()
+        const message = "Invalid username/password supplied"
+        cy.intercept('POST', '**/users/signin', {
+            statusCode: 422,
+            body: {
+                error: "Unprocessable Entity",
+                message: message,
+                path: "/users/signin",
+                status: 422,
+                timestamp: "2023-05-18T12:55:42.025+00:00"
+            }
+        })
+
+        // when
+        cy.login(user.username, user.password)
+
+        //
+        cy.get('.alert').should('have.text', message)
+    })
+
 
 })
