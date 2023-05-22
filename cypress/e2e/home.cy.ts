02ed38bfdafc2ce11b940205f979384d0568b65b
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-05-22 12:23:43 +0200

    klg06

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,13 +1,27 @@
 /// <reference types="cypress" />
 
-import { getUser } from "../utils/user"
+import { User, getUser } from "../utils/user"
 
 describe('Home page tests', () => {
+    let user: User
+
     beforeEach(() => {
-        const user = getUser()
+        user = getUser()
         cy.register(user)
         cy.login(user.username, user.password)
         cy.visit('http://localhost:8081')
+    })
+
+    afterEach(() => {
+        cy.getCookie('token').then(token => {
+            cy.request({
+                url: `http://localhost:4001/users/${user.username}`,
+                method: 'DELETE',
+                headers: {
+                    Authorization: `Bearer ${token?.value}`
+                }
+            })
+        })
     })
 
     it('should display at least one user', () => {
