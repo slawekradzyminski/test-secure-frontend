abe8f1081fa30e0017c5683474fb70a043c8e69a
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-05-22 14:49:14 +0200

    klg13

--- cypress/e2e/register.cy.ts
+++ cypress/e2e/register.cy.ts
@@ -4,21 +4,24 @@ import { getUser } from "../utils/user"
 
 describe('Login page tests', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8081')
+    cy.visit('http://localhost:8081/register')
   })
 
-  it('should successfully login', () => {
+  it('should fail to register if user already exists', () => {
     // given
     const user = getUser()
     cy.register(user)
 
     // when
     cy.get('[name=username]').type(user.username)
+    cy.get('[name=firstName]').type(user.firstName)
+    cy.get('[name=lastName]').type(user.lastName)
     cy.get('[name=password]').type(user.password)
+    cy.get('[name=email]').type(user.email)
     cy.get('.btn-primary').click()
 
     // then
-    cy.get('h1').should('contain.text', user.firstName)
+    cy.get('.alert').should('have.text', 'Username is already in use')
   })
 
 })
