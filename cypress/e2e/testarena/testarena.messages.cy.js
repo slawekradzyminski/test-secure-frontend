0bffe1d8e10e593d8123d958ca7de50d655c2220
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-09-02 14:06:31 +0200

    se06

---
+++ cypress/e2e/testarena/testarena.messages.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe("Test Arena my messages tests", () => {
+    beforeEach(() => {
+        cy.visit("http://demo.testarena.pl/zaloguj");
+    });
+
+    it('should successfully add new message', () => {
+
+    })
+});
---
+++ cypress/support/testarenacommands.js
@@ -0,0 +1,3 @@
+Cypress.Commands.add('loginToTestArena', (email, password) => {
+    
+})
