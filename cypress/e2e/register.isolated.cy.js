cb6666d39fbfe7e4ba093f8576f762ee85e2509b
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-09-03 15:46:01 +0200

    se26

--- cypress/e2e/register.isolated.cy.js
+++ cypress/e2e/register.isolated.cy.js
@@ -6,9 +6,9 @@ import { getRandomUser } from "./domain/user"
 
 describe('Register page tests', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:8081/register')
+        cy.visit('http://localhost:8081/register')
     })
-  
+
     it("should successfully register new user", () => {
         // given
         const user = getRandomUser()
@@ -27,5 +27,26 @@ describe('Register page tests', () => {
         cy.url().should('contain', '/login')
     })
 
-  })
-  
+    it("should show username already in use error message", () => {
+        // given
+        const user = getRandomUser()
+        const errorMessage = "Username is already in use"
+        cy.intercept('POST', '**/users/signup', {
+            statusCode: 422,
+            body: {
+                error: "Unprocessable Entity",
+                message: errorMessage,
+                path: "/users/signup",
+                status: 422,
+                timestamp: "2023-09-03T13:41:09.097+00:00"
+            }
+        })
+
+        // when
+        RegisterPage.attemptRegister(user)
+
+        // then
+        cy.get('.alert-danger').should("contain.text", errorMessage)
+    })
+
+})
