68b30f0507d8893a5d4c71e09ff2617bdac2fa77
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-10-14 15:02:14 +0200

    bury06

--- cypress/e2e/isolation/login.isolated.cy.ts
+++ cypress/e2e/isolation/login.isolated.cy.ts
@@ -1,7 +1,10 @@
 /// <reference types="cypress" />
 
+import { User } from "../../domain/User"
 import { getLoginResponseFor } from "../../domain/requests/loginTypes"
 import { getRandomUser } from "../../generators/userGenerator"
+import { getAllUsersMocks } from "../../mocks/getAllUsersMocks"
+import { loginMocks } from "../../mocks/loginMocks"
 
 describe('Login page tests in isolation', () => {
     beforeEach(() => {
@@ -11,19 +14,39 @@ describe('Login page tests in isolation', () => {
     it('should successfully login', () => {
         // given
         const user = getRandomUser()
-        cy.intercept('POST', '**/users/signin', {
-            statusCode: 200,
-            body: getLoginResponseFor(user)
-        })
-        cy.intercept('GET', '**/users', { fixture: 'users.json' })
+        loginMocks.mockSuccessfulLogin(user)
+        getAllUsersMocks.mockUsers()
 
         // when
         cy.get('[name=username]').type(user.username)
-        cy.get('[name=password]').type(user.lastName)
+        cy.get('[name=password]').type(user.password)
         cy.get('.btn-primary').click()
 
         // then
         cy.get('h1').should('contain.text', user.firstName)
+        verifyCorrectRequestWasBuild(user)
+    })
+
+    it('should fail to login', () => {
+        // given
+        const errorMessage = 'Invalid username/password supplied'
+        loginMocks.mockWrongCredentials(errorMessage)
+
+        // when
+        cy.get('[name=username]').type('wrong')
+        cy.get('[name=password]').type('wrong')
+        cy.get('.btn-primary').click()
+
+        // then
+        cy.get('.alert').should('contain.text', errorMessage)
+        cy.url().should('contain', '/login')
     })
 
 })
+
+const verifyCorrectRequestWasBuild = (user: User) => {
+    cy.get('@loginRequest').its('request.body').should('deep.equal', {
+        username: user.username,
+        password: user.password
+    })
+}
---
+++ cypress/mocks/httpMethods.ts
@@ -0,0 +1,5 @@
+export enum HttpMethods {
+    POST = 'POST',
+    GET = 'GET',
+    DELETE = 'DELETE'
+}
