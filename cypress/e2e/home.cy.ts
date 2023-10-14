8a50fe955eafc0d1fc1336b56dc1a9dc730f7fc6
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-10-14 11:20:33 +0200

    bory02

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Home page tests', () => {
+    beforeEach(() => {
+        cy.login('admin', 'admin')
+        cy.visit('http://localhost:8081')
+    })
+
+    it('should display at least one user', () => {
+        cy.get('li').should('have.length.at.least', 1)
+    })
+
+    it('should logout', () => {
+        cy.get('#logout').click()
+        cy.url().should('contain', '/login')
+    })
+
+    it('should open add more page', () => {
+        cy.get('#addmore').click()
+        cy.url().should('contain', '/add-user')
+    })
+
+})
