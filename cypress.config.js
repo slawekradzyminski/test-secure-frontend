8f3c4c5d3624b836c7b9a1bed174e0938941f4ff
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-10-28 14:32:46 +0200

    fit05

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    includeShadowDom: true,
+    env: {
+      isMobile: false
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
