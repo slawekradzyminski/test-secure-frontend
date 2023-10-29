6f8ec3f6b43bd2782c9ef06f085313c80b14cb4f
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-10-29 11:01:41 +0100

    fit13

--- cypress/e2e/edit.cy.js
+++ cypress/e2e/edit.cy.js
@@ -1,29 +1,38 @@
 /// <reference types="cypress" />
 
-import { getRandomEmail } from "../generator/user"
+import { getRandomEmail, getRandomUser } from "../generator/user"
 import { generateRandomString } from "../utils/random"
 
 describe('Edit page tests', () => {
+    let user
+
     beforeEach(() => {
-        cy.login('admin', 'admin')
-        cy.get('li').contains('Gosia Radzyminska').find('.edit').click()
+        user = getRandomUser()
+        cy.register(user)
+        cy.login(user.username, user.password)
+        cy.get('li').contains(`${user.firstName} ${user.lastName}`).find('.edit').click()
     })
 
     it('should correctly autofill data', () => {
-        cy.get("[name=firstName]").should('have.value', "Gosia")
-        cy.get("[name=lastName]").should('have.value', "Radzyminska")
-        cy.get("[name=email]").should('have.value', "client@email.com")
+        // then
+        cy.get("[name=firstName]").should('have.value', user.firstName)
+        cy.get("[name=lastName]").should('have.value', user.lastName)
+        cy.get("[name=email]").should('have.value', user.email)
     })
 
     it('should successfully edit an user', () => {
+        // given
         const newFirstName = generateRandomString(6)
         const newLastName = generateRandomString(6)
+
+        // when
         cy.get("[name=firstName]").clear().type(newFirstName)
         cy.get("[name=lastName]").clear().type(newLastName)
         cy.get("[name=email]").clear().type(getRandomEmail())
         cy.get('.btn-primary').click()
 
-        cy.get('li').contains('Gosia Radzyminska').should('not.exist')
+        // then
+        cy.get('li').contains(`${user.firstName} ${user.lastName}`).should('not.exist')
         cy.get('li').contains(`${newFirstName} ${newLastName}`).should('exist')
     })
 
--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,8 +1,14 @@
 /// <reference types="cypress" />
 
+import { getRandomUser } from "../generator/user"
+
 describe('Home page tests', () => {
+    let user
+
     beforeEach(() => {
-        cy.login('admin', 'admin')
+        user = getRandomUser()
+        cy.register(user)
+        cy.login(user.username, user.password)
     })
 
     it('should display at least one user', () => {
@@ -10,13 +16,13 @@ describe('Home page tests', () => {
     })
 
     it('should open edit user page', () => {
-        cy.get('li').contains('Gosia Radzyminska').find('.edit').click()
+        cy.get('li').contains(`${user.firstName} ${user.lastName}`).find('.edit').click()
 
         cy.get('h2').should('contain.text', 'Edit user')
     })
 
     it('should open email page', () => {
-        cy.get('li').contains('Gosia Radzyminska').find('.email').click()
+        cy.get('li').contains(`${user.firstName} ${user.lastName}`).find('.email').click()
 
         cy.get('h2').should('contain.text', 'Email user')
     })
