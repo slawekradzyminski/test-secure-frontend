67412db1055b9445b8da00dc8fb51121f3cbdc8c
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-12-16 15:52:06 +0100

    ken09

---
+++ cypress/e2e/isolation/home.cy.ts
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+import { getLoginResponseFrom } from "../../domain/api/login"
+import { getRandomUser } from "../../generator/userGenerator"
+import users from "../../fixtures/users.json"
+import { User } from "../../domain/user"
+
+let user: User
+
+describe('Home page tests in isolation', () => {
+    beforeEach(() => {
+        user = getRandomUser()
+        localStorage.setItem('user', JSON.stringify(getLoginResponseFrom(user)))
+        cy.setCookie('token', 'fakeToken')
+        cy.intercept('GET', '**/users', { fixture: 'users.json' })
+        cy.visit('')
+    })
+
+    it('should display correct data', () => {
+        cy.get('li').should('have.length', users.length)
+        cy.get('li').each(($row, i) => {
+            // Wolniejsza implementacja z cięzkim wrapem
+            // cy.wrap($row).should('contain.text', `${users[i].firstName} ${users[i].lastName}`)
+            expect($row.text()).to.contain(`${users[i].firstName} ${users[i].lastName}`)
+        })
+    })
+
+})
