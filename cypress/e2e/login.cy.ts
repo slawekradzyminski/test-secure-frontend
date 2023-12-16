2c76077c8747f58693d44bcbf8081e9a81dce74b
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-12-16 09:53:13 +0100

    ken01

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('example to-do app', () => {
+    beforeEach(() => {
+      cy.visit('')
+    })
+  
+    it('should login successfully', () => {
+      cy.get('[name=username]').type('admin')
+      cy.get('[name=password]').type('admin')
+      cy.get('.btn-primary').click()
+
+      cy.get('h1').contains('Slawomir')
+    })
+
+  })
+  
