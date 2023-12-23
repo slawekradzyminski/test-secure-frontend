0856f9881f48a1fd40b4674e254299dc9b109857
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-12-23 08:59:10 +0100

    increment

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  setupFiles: [
-    "./setupJest.js"
+  setupFilesAfterEnv: [
+    "./setupJest.ts"
   ]
 };
---
+++ setupJest.ts
@@ -0,0 +1,5 @@
+import fetchMock from 'jest-fetch-mock';
+import '@testing-library/jest-dom';
+
+(global.fetch as any) = fetchMock;
+fetchMock.enableMocks();
