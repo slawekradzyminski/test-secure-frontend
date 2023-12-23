6c900276c17defe8c403716f10511405b14739ce
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-12-24 00:20:25 +0100

    refactor

--- src/components/doctortypes/SpecialtyList.tsx
+++ src/components/doctortypes/SpecialtyList.tsx
@@ -1,26 +1,31 @@
-import { Box, FormControl, FormGroup, Grid, FormControlLabel, Checkbox, Button } from "@mui/material";
+import { Box, FormControl, FormGroup, Grid, FormControlLabel, Checkbox, Button, Typography } from "@mui/material";
 import React from "react";
 
 const SpecialtyList = ({ specialties, selectedSpecialties, handleChange, handleSubmit }) => (
-    <Box display="flex" justifyContent="center">
-        <FormControl component="fieldset">
-            <FormGroup>
-                <Grid container spacing={3}>
-                    {specialties.map((specialty, index) => (
-                        <Grid item xs={6} key={index}>
-                            <FormControlLabel
-                                control={<Checkbox checked={selectedSpecialties[specialty.doctorType] || false} onChange={handleChange} name={specialty.doctorType} />}
-                                label={specialty.doctorType}
-                            />
-                        </Grid>
-                    ))}
-                </Grid>
-            </FormGroup>
-            <Button variant="contained" color="primary" onClick={handleSubmit}>
-                Update My Specialties
-            </Button>
-        </FormControl>
-    </Box>
+    <>
+        <Typography variant="h4" align="center" gutterBottom mb={4}>
+            Please select your specialties
+        </Typography>
+        <Box display="flex" justifyContent="center">
+            <FormControl component="fieldset">
+                <FormGroup>
+                    <Grid container spacing={3}>
+                        {specialties.map((specialty, index) => (
+                            <Grid item xs={6} key={index}>
+                                <FormControlLabel
+                                    control={<Checkbox checked={selectedSpecialties[specialty.doctorType] || false} onChange={handleChange} name={specialty.doctorType} />}
+                                    label={specialty.doctorType}
+                                />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </FormGroup>
+                <Button variant="contained" color="primary" onClick={handleSubmit}>
+                    Update My Specialties
+                </Button>
+            </FormControl>
+        </Box>
+    </>
 );
 
 export default SpecialtyList
