65d1b6d042383329c41fcc2dc9adb6c716058619
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-03-24 08:15:48 +0100

    pom10

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    env: {
+      isMobile: false,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,1 +1,8 @@
 import './commands'
+
+beforeEach(() => {
+    cy.log('This will run before each test')
+    if (Cypress.env('isMobile')) {
+        cy.viewport('iphone-8')
+    }
+})
